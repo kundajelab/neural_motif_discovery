Fix NaN loss issue (numerical instability in softmax)
Also remove sigmoid normalization option

src/model/profile_models.py
@@ -197,42 +197,28 @@ def profile_tf_binding_predictor(
     return model
 
 
-def profile_logits_to_probs(logit_pred_profs, profile_norm_type="softmax"):
+def profile_logits_to_log_probs(logit_pred_profs):
     """
-    Converts the model's predicted profile logits into normalized probabilities.
+    Converts the model's predicted profile logits into normalized probabilities
+    via a softmax.
     Arguments:
         `logit_pred_profs`: a B x T x O x 2 tensor/array containing the
             predicted profile logits
-        `profile_norm_type`: how to normalize the logits to become
-            probabilities; can be "softmax" or "sigmoid"; for "softmax", simply
-            passes the logits through a softmax on the profile dimension; for
-            "sigmoid", passes everything through a sigmoid and then normalizes
     Returns a B x T x O x 2 tensor/array containing the predicted profiles as
-    probabilities. If the input is a tensor, the output will be a tensor. If the
-    input is a NumPy array, the output will be a NumPy array.
+    log probabilities. If the input is a tensor, the output will be a tensor. If
+    the input is a NumPy array, the output will be a NumPy array. Note that the
+    reason why this function returns log probabilities rather than raw
+    probabilities is for numerical stability.
     """
     if type(logit_pred_profs) is np.ndarray:
-        if profile_norm_type == "softmax":
-            return scipy.special.softmax(logit_pred_profs, axis=2)
-        else:
-            sig_pred_profs = scipy.special.expit(logit_pred_profs)
-            sig_sums = np.sum(sig_red_profs, axis=2, keepdims=True)
-            return np.divide(sig_pred_profs, sig_sums)
+        return logit_pred_profs - \
+            scipy.special.logsumexp(logit_pred_profs, axis=2, keepdims=True)
     else:
-        if profile_norm_type == "softmax":
-            return tf.nn.softmax(logit_pred_profs, axis=2)
-        else:
-            # We need to reimplement the sigmoid ourselves, otherwise the NaNs
-            # get lost (a known TF issue)
-            exp_pred_profs = tf.exp(logit_pred_profs)
-            sig_pred_profs = 1 / (1 + exp_pred_profs)
-            sig_sums = tf.reduce_sum(sig_pred_profs, axis=2, keep_dims=True)
-            return tf.divide(sig_pred_profs, sig_sums)
-
+        return logit_pred_profs - \
+            tf.reduce_logsumexp(logit_pred_profs, axis=2, keep_dims=True)
 
 def profile_loss(
-    true_prof_counts, logit_pred_profs, num_tasks, profile_length,
-    profile_norm_type="softmax"
+    true_prof_counts, logit_pred_profs, num_tasks, profile_length
 ):
     """
     Returns the loss of the correctness off the predicted profiles. The profile
@@ -248,14 +234,11 @@ def profile_loss(
             profile _logits_
         `num_tasks`: the number of tasks T
         `profile_length`: the length of the profile outputs O
-        `profile_norm_type`: how to normalize the logits to become
-            probabilities; can be "softmax" or "sigmoid"
     Returns a scalar loss tensor.
     """
     # Convert logits to log probabilities
-    log_pred_profs = tf.log(
-        profile_logits_to_probs(logit_pred_profs, profile_norm_type)
-    ) # Shape: B x T x O x 2
+    log_pred_profs = profile_logits_to_log_probs(logit_pred_profs)
+    # Shape: B x T x O x 2
 
     # Reshape the inputs to be flat along the tasks dimension
     true_prof_counts = tf.reshape(
@@ -272,12 +255,12 @@ def profile_loss(
 
     # Compute probability of seeing true profile under distribution of log
     # predicted probs
-    log_probs = multinomial_log_probs(
+    log_likely = multinomial_log_probs(
         log_pred_profs, true_counts, true_prof_counts
     )
 
     # Average the loss across tasks/strands, then across the batch
-    batch_prof_loss = tf.reduce_mean(-log_probs, axis=1)
+    batch_prof_loss = tf.reduce_mean(-log_likely, axis=1)  # Neg. log-likelihood
     prof_loss = tf.reduce_mean(batch_prof_loss)
 
     return prof_loss

src/model/train_profile_model.py
@@ -52,9 +52,6 @@ def config(dataset):
     # Number of prediction tasks (2 outputs for each task: plus/minus strand)
     num_tasks = 4
 
-    # Type of normalization for profile probabilities
-    profile_norm_type = "softmax"
-
     # Amount to weight the counts loss within the correctness loss
     counts_loss_weight = 100
 
@@ -62,7 +59,7 @@ def config(dataset):
     num_epochs = 10
 
     # Learning rate
-    learning_rate = 0.001
+    learning_rate = 0.004
 
     # Whether or not to use early stopping
     early_stopping = True
@@ -94,14 +91,15 @@ def config(dataset):
     # Imported from make_profile_dataset
     num_workers = 10
 
-def get_profile_loss_function(num_tasks, profile_length, profile_norm_type):
+
+def get_profile_loss_function(num_tasks, profile_length):
     """
     Returns a _named_ profile loss function. When saving the model, Keras will
     use "profile_loss" as the name for this loss function.
     """
     def profile_loss(true_vals, pred_vals):
         return profile_models.profile_loss(
-            true_vals, pred_vals, num_tasks, profile_length, profile_norm_type
+            true_vals, pred_vals, num_tasks, profile_length
         )
     return profile_loss
 
@@ -120,8 +118,7 @@ def get_count_loss_function(num_tasks):
 def create_model(
     input_length, input_depth, profile_length, num_tasks, num_dil_conv_layers,
     dil_conv_filter_sizes, dil_conv_stride, dil_conv_dilations, dil_conv_depths,
-    prof_conv_kernel_size, prof_conv_stride, counts_loss_weight, learning_rate,
-    profile_norm_type
+    prof_conv_kernel_size, prof_conv_stride, counts_loss_weight, learning_rate
 ):
     """
     Creates and compiles profile model using the configuration above.
@@ -143,9 +140,7 @@ def create_model(
     prof_model.compile(
         keras.optimizers.Adam(lr=learning_rate),
         loss=[
-            get_profile_loss_function(
-                num_tasks, profile_length, profile_norm_type
-            ),
+            get_profile_loss_function(num_tasks, profile_length),
             get_count_loss_function(num_tasks),
         ],
         loss_weights=[1, counts_loss_weight]
@@ -193,7 +188,7 @@ def train_epoch(
 
 
 @train_ex.capture
-def eval_epoch(val_queue, val_batch_num, model, num_tasks, profile_norm_type):
+def eval_epoch(val_queue, val_batch_num, model, num_tasks):
     """
     Runs the data from the validation data queue once through the model. Returns
     a list of losses for the batches. Note that the queue is expected to return
@@ -240,9 +235,10 @@ def eval_epoch(val_queue, val_batch_num, model, num_tasks, profile_norm_type):
     true_total_counts = np.concatenate(true_total_counts)
 
     # Convert the model output logits and logs to desired values
-    pred_prof_probs = profile_models.profile_logits_to_probs(
-        logit_pred_profs, profile_norm_type
+    pred_prof_log_probs = profile_models.profile_logits_to_log_probs(
+        logit_pred_profs
     )
+    pred_prof_probs = np.exp(pred_prof_log_probs)
     pred_counts = np.exp(log_pred_counts) - 1
     
     # Compute performance on validation set