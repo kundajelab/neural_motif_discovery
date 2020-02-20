04e54541f64a0252bdd846aee7473a99e009de7e
Alex Tseng <amtseng@stanford.edu>
2020-02-20 10:54:00 -0800

    Update script to compute SHAP scores

--- src/extract/compute_shap.py
+++ src/extract/compute_shap.py
@@ -15,8 +15,8 @@ def create_background(model_inputs, bg_size=10, seed=20191206):
     Returns a pair of arrays as a list, where the first array is G x I x 4, and
     the second array is G x T x O x 2; these are the background inputs. The
     background for the input sequences is randomly dinuceotide-shuffles of the
-    original sequence. The background for the control profiles is the same as the
-    originals.
+    original sequence. The background for the control profiles is the same as
+    the originals.
     """
     input_seq, cont_profs = model_inputs
     input_seq_bg = np.empty((bg_size,) + input_seq.shape)
@@ -31,12 +31,12 @@ def create_background(model_inputs, bg_size=10, seed=20191206):
 
 def combine_mult_and_diffref(mult, orig_inp, bg_data):
     """
-    Computes the hypothetical contribution of any base in the input to the
-    output, given the multipliers for the background data. This will simulate
-    all possible base identities and compute a separate "difference-from-
-    reference" for each, averaging the product of the multipliers with these
-    differences, over the base identities. For the control profiles, the
-    returned contribution is 0.
+    Computes the hypothetical contribution of any base along the input sequence
+    to the final output, given the multipliers for the input sequence
+    background. This will simulate all possible base identities as compute a
+    "difference-from-reference" for each possible base, averaging the product
+    of the multipliers with the differences, over the base identities. For the
+    control profiles, the returned contribution is 0.
     Arguments:
         `mult`: multipliers for the background data; a pair of a G x I x 4 array
             and a G x T x O x 2 array
@@ -46,26 +46,30 @@ def combine_mult_and_diffref(mult, orig_inp, bg_data):
             G x T x O x 2 array
     Returns a pair of importance scores as a list: an I x 4 array and a
     T x O x 2 zero-array.
-    Note that this rule is necessary because by default, the multipliers are
-    multiplied by the difference-from-reference (for each reference in the
-    background set). However, using the actual sequence as the target would not
-    allow getting hypothetical scores, as the resulting attributions use a
-    difference-from-reference wherever the target does _not_ have that base.
-    Thus, we compute the hypothetical scores manually by trying every possible
-    base as the target (instead of using the actual original target input).
-    To back-out the actual scores for the original target input, simply extract
-    the entries for the bases in the real input.
+    This function is necessary for this specific implementation of DeepSHAP. In
+    the original DeepSHAP, the final step is to take the difference of the input
+    sequence to each background sequence, and weight this difference by the
+    contribution multipliers for the background sequence. However, all
+    differences to the background would be only for the given input sequence
+    (i.e. the actual importance scores). To get the hypothetical importance
+    scores efficiently, we try every possible base for the input sequence, and
+    for each one, compute the difference-from-reference and weight by the
+    multipliers separately. This allows us to compute the hypothetical scores
+    in just one pass, instead of running DeepSHAP many times. To get the actual
+    scores for the original input, simply extract the entries for the bases in
+    the real input sequence.
     """
-    input_mult, cont_profs_mult = mult
+    # Reassign arguments to better names; this specific implementation of
+    # DeepSHAP requires the arguments to have the above names
+    input_seq_bg_mults, cont_profs_bg_mults = mult
     input_seq, cont_profs = orig_inp
     input_seq_bg, cont_profs_bg = bg_data
 
-    cont_profs_hyp_scores = np.zeros_like(cont_profs)
     # Allocate array to store hypothetical scores, one set for each background
-    # reference
-    input_seq_hyp_scores_eachref = np.empty_like(input_seq_bg)
+    # reference (i.e. each difference-from-reference)
+    input_seq_hyp_scores_eachdiff = np.empty_like(input_seq_bg)
     
-    # Loop over input bases
+    # Loop over the 4 input bases
     for i in range(input_seq.shape[-1]):
         # Create hypothetical input of all one type of base
         hyp_input_seq = np.zeros_like(input_seq)
@@ -76,19 +80,20 @@ def combine_mult_and_diffref(mult, orig_inp, bg_data):
         # Shape: G x I x 4
 
         # Weight difference-from-reference by multipliers
-        contrib = diff_from_ref * input_mult
+        contrib = diff_from_ref * input_seq_bg_mults
 
-        # Sum across bases axis; this computes the hypothetical score AS IF the
+        # Sum across bases axis; this computes the actual importance score AS IF
         # the target sequence were all that base
-        input_seq_hyp_scores_eachref[:, :, i] = np.sum(contrib, axis=-1)
+        input_seq_hyp_scores_eachdiff[:, :, i] = np.sum(contrib, axis=-1)
 
-    # Average hypothetical scores across background references
-    input_seq_hyp_scores = np.mean(input_seq_hyp_scores_eachref, axis=0)
-
+    # Average hypothetical scores across background
+    # references/diff-from-references
+    input_seq_hyp_scores = np.mean(input_seq_hyp_scores_eachdiff, axis=0)
+    cont_profs_hyp_scores = np.zeros_like(cont_profs)  # All 0s
     return [input_seq_hyp_scores, cont_profs_hyp_scores]
+    
 
-
-def create_explainer(model, task_index=None, output_type="profile"):
+def create_explainer(model, task_index=None):
     """
     Given a trained Keras model, creates a Shap DeepExplainer that returns
     hypothetical scores for the input sequence.
@@ -97,51 +102,35 @@ def create_explainer(model, task_index=None, output_type="profile"):
         `task_index`: a specific task index (0-indexed) to perform explanations
             from (i.e. explanations will only be from the specified outputs); by
             default explains all tasks
-        `output_type`: if "profile", utilizes the profile output to compute the
-            importance scores; if "count", utilizes the counts output; in either
-            case, the importance scores are for the ouputs summed across strands
-            and tasks.
     Returns a function that takes in input sequences and control profiles, and
     outputs hypothetical scores for the input sequences.
     """
-    assert output_type in ("profile", "count")
-    if output_type == "profile":
-        prof_output = model.output[0]  # Shape: B x T x O x 2 (logits)
-       
-        # As a slight optimization, instead of explaining the logits, explain
-        # the logits weighted by the probabilities after passing through the
-        # softmax; this exponentially increases the weight for high-probability
-        # positions, and exponentially reduces the weight for low-probability
-        # positions, resulting in a more cleaner signal
+    prof_output = model.output[0]  # Shape: B x T x O x 2 (logits)
+    
+    # As a slight optimization, instead of explaining the logits, explain
+    # the logits weighted by the probabilities after passing through the
+    # softmax; this exponentially increases the weight for high-probability
+    # positions, and exponentially reduces the weight for low-probability
+    # positions, resulting in a more cleaner signal
 
-        # First, center/mean-normalize the logits so the contributions are
-        # normalized, as a softmax would do
-        logits = prof_output - \
-            tf.reduce_mean(prof_output, axis=2, keepdims=True)
+    # First, center/mean-normalize the logits so the contributions are
+    # normalized, as a softmax would do
+    logits = prof_output - \
+        tf.reduce_mean(prof_output, axis=2, keepdims=True)
 
-        # Stop gradients flowing to softmax, to avoid explaining those
-        logits_stopgrad = tf.stop_gradient(logits)
-        probs = tf.nn.softmax(logits_stopgrad, axis=2)
+    # Stop gradients flowing to softmax, to avoid explaining those
+    logits_stopgrad = tf.stop_gradient(logits)
+    probs = tf.nn.softmax(logits_stopgrad, axis=2)
 
-        logits_weighted = logits * probs  # Shape: B x T x O x 2
-        if task_index:
-            logits_weighted = logits_weighted[:, task_index : task_index + 1]
-        prof_sum = tf.reduce_sum(logits_weighted, axis=(1, 2, 3))
-        explainer = shap.DeepExplainer(
-            model=(model.input, prof_sum),
-            data=create_background,
-            combine_mult_and_diffref=combine_mult_and_diffref
-        )
-    else:
-        count_output = model.output[1]  # Shape: B x T x 2
-        if task_index:
-            count_output = count_output[:, task_index : task_index + 1]
-        count_sum = tf.reduce_sum(count_output, axis=(1, 2))
-        explainer = shap.DeepExplainer(
-            model=(model.input, count_sum),
-            data=create_background,
-            combine_mult_and_diffref=combine_mult_and_diffref
-        )
+    logits_weighted = logits * probs  # Shape: B x T x O x 2
+    if task_index:
+        logits_weighted = logits_weighted[:, task_index : task_index + 1]
+    prof_sum = tf.reduce_sum(logits_weighted, axis=(1, 2, 3))
+    explainer = shap.DeepExplainer(
+        model=(model.input, prof_sum),
+        data=create_background,
+        combine_mult_and_diffref=combine_mult_and_diffref
+    )
 
     def explain_fn(input_seqs, cont_profs):
         """
@@ -170,7 +159,7 @@ if __name__ == "__main__":
     from deeplift.visualization import viz_sequence
 
     files_spec_path = "/users/amtseng/tfmodisco/data/processed/ENCODE/config/SPI1/SPI1_training_paths.json"
-    model_path = "/users/amtseng/tfmodisco/models/trained_models/SPI1/35/model_ckpt_epoch_6.h5"
+    model_path = "/users/amtseng/tfmodisco/models/trained_models/SPI1_fold7/1/model_ckpt_epoch_8.h5"
     
     reference_fasta = "/users/amtseng/genomes/hg38.fasta"
     chrom_sizes = "/users/amtseng/genomes/hg38.canon.chrom.sizes"
@@ -181,8 +170,9 @@ if __name__ == "__main__":
     # Extract files
     with open(files_spec_path, "r") as f:
         files_spec = json.load(f)
-    val_peak_beds = files_spec["val_peak_beds"]
-    prof_bigwigs = files_spec["prof_bigwigs"]
+    peak_beds = files_spec["peak_beds"]
+    profile_hdf5 = files_spec["profile_hdf5"]
+    chrom_set = ["chr21"]
 
     # Import model
     model = train_profile_model.load_model(
@@ -191,11 +181,11 @@ if __name__ == "__main__":
 
     # Make data loader
     data_loader = make_profile_dataset.create_data_loader(
-        val_peak_beds, prof_bigwigs[num_tasks:], "SummitCenteringCoordsBatcher",
-        batch_size=128, reference_fasta=reference_fasta,
-        chrom_sizes=chrom_sizes, input_length=input_length,
-        profile_length=profile_length, negative_ratio=0, peak_tiling_stride=0,
-        revcomp=False, jitter_size=0, dataset_seed=0, shuffle=False,
+        peak_beds, profile_hdf5, "SummitCenteringCoordsBatcher", batch_size=128,
+        reference_fasta=reference_fasta, chrom_sizes=chrom_sizes,
+        input_length=input_length, profile_length=profile_length,
+        negative_ratio=0, peak_tiling_stride=0, revcomp=False, jitter_size=0,
+        dataset_seed=None, chrom_set=chrom_set, shuffle=False,
         return_coords=True
     )
     enq = keras.utils.OrderedEnqueuer(data_loader, use_multiprocessing=True)
@@ -204,18 +194,15 @@ if __name__ == "__main__":
     para_batch_gen = enq.get()
 
     # Make explainers
-    prof_explainer = create_explainer(model, output_type="profile")
-    count_explainer = create_explainer(model, output_type="count")
+    prof_explainer = create_explainer(model)
 
     # Compute importance scores
     prof_scores = []
-    count_scores = []
     all_input_seqs, all_coords = [], []
     for i in tqdm.trange(len(enq.sequence)):
-        input_seqs, cont_profs, status, coords, peaks = next(para_batch_gen)
-
+        input_seqs, profiles, status, coords, peaks = next(para_batch_gen)
+        cont_profs = profiles[:, num_tasks:]
         prof_scores.append(prof_explainer(input_seqs, cont_profs))
-        count_scores.append(count_explainer(input_seqs, cont_profs))
         all_input_seqs.append(input_seqs)
         all_coords.append(coords)
         break
@@ -223,12 +210,11 @@ if __name__ == "__main__":
     enq.stop()
 
     prof_scores = np.concatenate(prof_scores, axis=0)  
-    count_scores = np.concatenate(count_scores, axis=0)
     input_seqs = np.concatenate(all_input_seqs, axis=0)
     coords = np.concatenate(all_coords, axis=0)
 
     # Plot a pair of hypothetical and actual importance scores
-    viz_sequence.plot_weights(prof_scores[0], subticks_frequency=100)
+    viz_sequence.plot_weights(prof_scores[0][650:750], subticks_frequency=100)
     viz_sequence.plot_weights(
-        prof_scores[0] * input_seqs[0], subticks_frequency=100
+        (prof_scores[0] * input_seqs[0])[650:750], subticks_frequency=100
     )
